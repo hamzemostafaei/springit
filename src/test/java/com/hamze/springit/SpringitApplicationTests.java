58cbb6d443e9f7f6967a2f113f2fdb5e0c9cfcb7
Mostafaei <hamzemostafaei@yahoo.com>
2020-07-03 00:37:13 +0430

    Commit initial spring boot project!

---
+++ src/test/java/com/hamze/springit/SpringitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamze.springit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
